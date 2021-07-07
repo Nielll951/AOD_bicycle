3f0b3f0b92501a800d80194f7a17e884ad5117be
Kostya <rodina9180gaocheng@ukr.net>
2021-07-07 14:24:36 +0300

    completed menu and adapted it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$('.icon-menu').click(function(event) {
+   $(this).toggleClass('active');
+   $('.menu__body').toggleClass('active');
+   $('body').toggleClass('lock');
+});
+
 function ibg() {
    $.each($('.ibg'), function(index, val) {
       if($(this).find('img').length>0) {
