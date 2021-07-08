fcf486833ad88fb0b9fe843f34d5b3ac25a4aa08
Kostya <rodina9180gaocheng@ukr.net>
2021-07-08 21:27:37 +0300

    defined section shop

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ function ibg() {
    });
 }
 ibg();
+
+// SLIDER
+if($('.slider__body').length>0) {
+   $('.slider__body').slick({
+      dots: true,
+      arrows: false,
+      accessibility: false,
+      slidesToShow: 1,
+      autoplaySpeed: 3000,
+      adaptiveHeight: true,
+      nextArrow: '<button type="button" class="slick-next"></button>',
+      nextArrow: '<button type="button" class="slick-prev"></button>',
+      responsive: [{
+         breakpoints: 768,
+         settings: {}
+      }]
+   });
+}
