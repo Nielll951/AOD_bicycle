970ee5f91addfe346e52a6587f5f38b1498b2956
Kostya <rodina9180gaocheng@ukr.net>
2021-07-11 12:28:46 +0300

    completed the project

--- js/script.js
+++ js/script.js
@@ -14,19 +14,28 @@ function ibg() {
 ibg();
 
 // SLIDER
-if($('.slider__body').length>0) {
+$(document).ready(function() {
    $('.slider__body').slick({
-      dots: true,
       arrows: false,
-      accessibility: false,
-      slidesToShow: 1,
-      autoplaySpeed: 3000,
-      adaptiveHeight: true,
-      nextArrow: '<button type="button" class="slick-next"></button>',
-      nextArrow: '<button type="button" class="slick-prev"></button>',
-      responsive: [{
-         breakpoints: 768,
-         settings: {}
-      }]
+      dots: true,
+      adaptiveHeight: true
    });
-}
+});
+
+
+// if($('.slider__body').length>0) {
+//    $('.slider__item').slick({
+//       dots: true,
+//       arrows: false,
+//       accessibility: false,
+//       slidesToShow: 1,
+//       autoplaySpeed: 3000,
+//       adaptiveHeight: true,
+//       nextArrow: '<button type="button" class="slick-next"></button>',
+//       nextArrow: '<button type="button" class="slick-prev"></button>',
+//       responsive: [{
+//          breakpoints: 768,
+//          settings: {}
+//       }]
+//    });
+// }
